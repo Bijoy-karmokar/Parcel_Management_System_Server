d8f2bdb49bf3df8236eb70c5a05de57797a1aaef
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-01-04 13:52:02 +0600

    specific parcels

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -30,21 +30,57 @@ async function run() {
 
     app.get("/parcels", async (req, res) => {
       const parcels = await parcelCollection.find().toArray();
-      
+
       res.send(parcels);
     });
+    // get myParcels
+    app.get("/parcels", async (req, res) => {
+      try {
+        const userEmail = req.query.email;
+        const query = userEmail ? { created_by: userEmail } : {};
+        const options = {
+          sort: { createdAt: -1 },
+        };
+        const parcels = await parcelCollection.find(query, options).toArray();
+        res.send(parcels);
+      } catch (error) {
+        res.status(500).send({ message: "Failed to get parcels" });
+      }
+    });
 
-    // post the parcels
-   app.post("/parcels", async (req, res) => {
-  const parcel = req.body;
-  const result = await parcelCollection.insertOne(parcel);
-  res.send({
-    success: true,
-    insertedId: result.insertedId,
-  });
+    // get a specific parcelID
+app.get("/parcels/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  
+  try {
+    const parcel = await parcelCollection.findOne({ _id: new ObjectId(id) });
+    if (!parcel) return res.status(404).send({ message: "Parcel not found" });
+    res.send(parcel);
+  } catch (err) {
+    res.status(500).send({ message: "Invalid parcel ID" });
+  }
 });
 
+    // post the parcels
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      const result = await parcelCollection.insertOne(parcel);
+      res.send({
+        success: true,
+        insertedId: result.insertedId,
+      });
+    });
 
+    // delete the parcels
+
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await parcelCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
